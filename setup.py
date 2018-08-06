239e1d27d0547dc0370c2a778a513e62054efff5
Sunny <463271945@qq.com>
2018-08-06 17:42:46 +0800

    调整目录结构，重构代码

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+from setuptools import setup
+
+setup(name='analysispcap',
+      version='0.1',
+      description='对pcap文件进行解析，解析出tcp数据',
+      url='https://github.com/sunpudding/python',
+      author='sunpudding',
+      author_email='463271945@qq.com',
+      license='MIT',
+      packages=['analysispcap'],
+      install_requires=[
+          'struct',
+            'argparse',
+
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
