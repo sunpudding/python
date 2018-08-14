5bf8f8d8cc82612156a2b62c8a84c2ffed0ded6e
Sunny <463271945@qq.com>
2018-08-14 16:16:05 +0800

    新增获取tcp流功能

--- analysispcap/analysisPcap.py
+++ analysispcap/analysisPcap.py
@@ -29,6 +29,9 @@ class AnalysisPcap(object):
 
     @staticmethod
     def get_tcp_data(data):
+        """传入数据帧，对数据帧的Tcp中的应用层数据进行获取
+
+        返回该tcp数据"""
         ip_header_len = (data[14] & 0x0F) * 4
         ip_total_len = struct.unpack(
             '!H', data[16: 18])[0]
@@ -37,6 +40,39 @@ class AnalysisPcap(object):
                         tcp_header_len:14 + ip_total_len]
         return tcontent
 
+    def appoint_tcp_stream(
+            self,
+            data,
+            source,
+            destinate,
+            source_port,
+            destinate_port):
+        """指定tcp流
+
+        :param data: 传入数据帧
+        :param source: 传入源ip地址
+        :param destinate: 传入目的ip地址
+        :param source_port: 传入源tcp端口
+        :param destinate_port: 传入目的tcp端口
+        :return: tcp流以字典的形式 {Sequence number:Tcp content}
+        """
+        # 对ip地址进行拼接(XXX.XXX.XXX.XXX)
+        ip_source = '.'.join([str(i) for i in data[26:30]])
+        ip_destinate = '.'.join([str(i) for i in data[30:34]])
+        ip_header_len = (data[14] & 0x0F) * 4
+        tcp_source = struct.unpack(
+            '!H', data[14 + ip_header_len:14 + ip_header_len + 2])[0]
+        tcp_destinate = struct.unpack(
+            '!H', data[14 + ip_header_len + 2:14 + ip_header_len + 4])[0]
+        tcp_stream = dict()
+        if ip_source == source and ip_destinate == destinate:
+            if tcp_source == source_port and tcp_destinate == destinate_port:
+                seq = struct.unpack(
+                    '!I', data[14 + ip_header_len + 4: 14 + ip_header_len + 8])[0]
+                content = self.get_tcp_data(data)
+                tcp_stream[seq] = content
+        return tcp_stream
+
     def dump_tcp_content(self):
         """传入pcap文件，导出 PCAP 文件中的 TCP 内容
 
@@ -44,8 +80,10 @@ class AnalysisPcap(object):
         open_file = open(self.pcap_path, 'rb')
         file_length = int(open_file.seek(0, io.SEEK_END))
         open_file.seek(24)
-        tcp_content = []
+        tcp_content, tcp_stream = [], dict()
         pcap_header = 24
+        source, destinate = '192.168.43.158', '183.232.24.222'
+        source_port, destinate_port = 64343, 80
         while pcap_header < file_length:
             # Packet header, len=16
             open_file.seek(8, io.SEEK_CUR)
@@ -55,6 +93,9 @@ class AnalysisPcap(object):
             pkt_body = open_file.read(pkt_length)
             if self.is_ipv4(pkt_body):
                 if self.is_tcp(pkt_body):
+                    stream = self.appoint_tcp_stream(
+                        pkt_body, source, destinate, source_port, destinate_port)
+                    tcp_stream.update(stream)
                     tcontent = self.get_tcp_data(pkt_body)
                     tcp_content.append(tcontent)
                 else:
@@ -76,6 +117,7 @@ class AnalysisPcap(object):
                 content = 'TCP的应用层数据:{}\n'.format(data)
                 tcp_content.write(content)
         tcp_content.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
